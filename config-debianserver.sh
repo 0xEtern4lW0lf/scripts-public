8b3a79006f6f60a052f7a38c0e05ffb83692626d
racksalmar <80726207+racksalmar@users.noreply.github.com>
2022-05-14 14:07:40 -0300

    Update config-debianserver.sh

--- config-debianserver.sh
+++ config-debianserver.sh
@@ -71,10 +71,11 @@ AptFix(){
     apt clean -y
 }
 #----------------------------------------------------------------------------------#
+# Configurações Pessoal
 ConfPessoal(){
   # Bashrc personalizado
   for i in $(ls /home); do
-  mv -f ./config/bashrc/user-bashrc $i/.bashrc
+  mv -f ./config/bashrc/user-bashrc /home/$i/.bashrc
   done
   mv -f ./config/bashrc/root-bashrc /root/.bashrc
   # Banner personalizado
@@ -96,7 +97,48 @@ ConfPessoal(){
     # Interfaces
     mv -f ./config/interfaces /etc/network/interfaces
     # Hosts
-    
+    mv -f ./config/hosts /etc/hosts
+    # Resolv.conf
+    echo -e "nameserver 8.8.8.8 \nnameserver8.8.4.4"
+}
+#----------------------------------------------------------------------------------#
+# Configuração dos Programas
+ConfProgram(){
+    # Fail2ban
+    echo -e "bantime = 360m"  > /etc/fail2ban/jail.d/pessoal.conf
+    echo -e "findtime = 10m/g" >> /etc/fail2ban/jail.d/pessoal.conf
+    echo -e "maxretry = 3" >> /etc/fail2ban/jail.d/pessoal.conf
+    # PSAD
+    sed -i 's/_CHANGEME_/debian-modelo/' etc/psad/psad.conf
+    # ZABBIX AGENT
+    sed -i 's/Server=127.0.0.1/Server=192.168.56.4/' /etc/zabbix/zabbix_agentd.conf
+    sed -i 's/ServerActive=127.0.0.1/ServerActive=192.168.56.4/' /etc/zabbix/zabbix_agentd.conf
+    # Rsyslog
+    sed -i 's/#module(load="imudp")/module(load="imudp")/' /etc/rsyslog.conf
+    sed -i 's/#input(type="imudp" port="514")/input(type="imudp" port="514")/' /etc/rsyslog.conf
+    echo -e "\n\n#### PERSONAL RULES ####" >> /etc/rsyslog.conf
+    echo -e "\n*.* @192.168.56.5:514" >> /etc/rsyslog.conf
+    echo -e '\n$ActionQueueFileName queue' >> /etc/rsyslog.conf
+    echo -e '$ActionQueueMaxDiskSpace 2g' >> /etc/rsyslog.conf
+    echo -e '$ActionQueueSaveonShutdown on' >> /etc/rsyslog.conf
+    echo -e '$ActionQueueType LinkedList' >> /etc/rsyslog.conf
+    echo -e '$ActionResumeRetryCount -1' >> /etc/rsyslog.conf
+    # SCREEN
+    sed -i 's/#startup_message off/startup_message off/' /etc/screenrc
+    echo "screen" >> /root/.profile
+    # SCREENFETCH
+    echo "if [ -f /usr/bin/screenfetch ]; then screenfetch; fi" >> /etc/bash.bashrc
+    # RKHUNTER
+    echo """#!/bin/bash
+rkhunter --update
+rkhunter --propudp
+rkhunter --checkall --sk""" > /usr/local/bin/rkhunter.sh
+    chmod 755 /usr/local/bin/rkhunter.sh
+}
+#----------------------------------------------------------------------------------#
+# Reiniciar o sistema
+Reiniciar(){
+rm -rf ../scripts/; reboot
 }
 #----------------------------------------------------------------------------------#
 Inicio(){
@@ -111,10 +153,19 @@ echo -e "\n###### Script Encerrado em $(date +%d%H%M%b%y) #####" >> $HOME/log-sc
 echo -e "\n#----------------------------------------------------------------------------------#" >> $HOME/log-script.log
 }
 #==================================================================================#
-
+## Verificando usuário
+CheckUser
 
 ##### EXECUÇAO #####
 Inicio
+
+Atualizar
+
+InstalarPrograma
+
+ConfProgram
+
+Reiniciar
 
 ##### FINALIZAÇAO #####
 Final

